754c8400e215b9d6059c881498284cd88e950b96
voidstar240 <andrewfreuler@gmail.com>
2023-09-14 22:53:34 -0400

    Finish question 2

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <iomanip>
 #include <iostream>
+#include <ostream>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -33,6 +34,21 @@ double pilot_roll_rate_dot(RollState state) {
     // p_dot = L_p * p + L_del * delta
     const double roll_rate_dot =
           roll_damping * state.roll_rate + aileron_eff * pilot_aileron_deflection(state);
+    return roll_rate_dot;
+}
+
+double auto_aileron_deflection(RollState state) {
+    const double k = 1.5;
+    return -k * state.roll_rate;
+}
+
+double auto_roll_rate_dot(RollState state) {
+    const double roll_damping = -0.8;
+    const double aileron_eff = 0.9;
+
+    // p_dot = L_p * p + L_del * delta
+    const double roll_rate_dot =
+          roll_damping * state.roll_rate + aileron_eff * auto_aileron_deflection(state);
     return roll_rate_dot;
 }
 
@@ -71,7 +87,11 @@ void csv_print_roll_states(std::vector<RollState> roll_states) {
     std::cout << std::endl;
 }
 
-int main() {
+void question_1() {
+    std::cout << "==================\n";
+    std::cout << "=== Question 1 ===\n";
+    std::cout << "==================\n" << std::endl;
+
     // calc integration bounds
     const double t_initial = 0.0;
     const double t_final = 5.0;
@@ -88,4 +108,36 @@ int main() {
     // print roll states
     pretty_print_roll_states(roll_states);
     // csv_print_roll_states(roll_states);
+
+    std::cout << "\n" << std::endl;
+}
+
+void question_2() {
+    std::cout << "==================\n";
+    std::cout << "=== Question 2 ===\n";
+    std::cout << "==================\n" << std::endl;
+
+    // calc integration bounds
+    const double t_initial = 0.0;
+    const double t_final = 5.0;
+    const double delta_t = 0.01;
+    const uint64_t step_num = std::ceil(1.0 + (t_final - t_initial) / delta_t);
+
+    // set up initial conditions
+    std::vector<RollState> roll_states = std::vector(step_num, RollState());
+    roll_states[0] = RollState(0.0, 1.0);
+
+    // integrate
+    roll_rate_prediction(auto_roll_rate_dot, roll_states, delta_t);
+
+    // print roll states
+    pretty_print_roll_states(roll_states);
+    // csv_print_roll_states(roll_states);
+
+    std::cout << "\n" << std::endl;
+}
+
+int main() {
+    question_1();
+    question_2();
 }
