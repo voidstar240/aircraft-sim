96168efa7bf69f3d6fe30305be2b3387ebe357d7
voidstar240 <andrewfreuler@gmail.com>
2023-11-10 21:37:17 -0500

    Implemented Graph for storing airports and distances

---
+++ src/graph.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <vector>
+
+typedef size_t NodeID;
+template <typename NodeData, typename EdgeData>
+class Graph {
+	struct Edge {
+		NodeID from;
+		NodeID to;
+		EdgeData data;
+	};
+	std::vector<NodeData> nodes;
+	std::vector<Edge> edges;
+
+public:
+	/// Create an empty graph.
+	Graph() : nodes(), edges() {}
+
+	/// Create a grpah from a list of nodes.
+	Graph(std::initializer_list<NodeData> nodes) : nodes(nodes), edges() {}
+
+	/// Add `node` to the graph.
+	void AddNode(const NodeData& node) {
+		this->nodes.push_back(node);
+	}
+
+	/// Creates an edge between nodes `from` and `to` consisting of data `edge`.
+	void AddDirectionalEdge(const NodeID from, const NodeID to, const EdgeData& edge) {
+		this->edges.push_back({ .from = from, .to = to, .data = edge });
+	}
+
+	/// Creates two edges between nodes `from` and `to`, and vice-versa consisting of data `edge`. 
+	void AddNonDirectionalEdge(const NodeID from, const NodeID to, const EdgeData& edge) {
+		this->edges.push_back({ .from = from, .to = to, .data = edge });
+		this->edges.push_back({ .from = to, .to = from, .data = edge });
+	}
+
+	/// Returns false if no node matches `node` leaving `outNodeID` untouched.
+	/// If a node matching `node` does exist, true is returned and `outNodeID`
+	/// is set to the NodeID of the matching node.
+	bool FindNodeID(const NodeData& node, NodeID& outNodeID) {
+		for (size_t i = 0; i < this->nodes.size(); i++) {
+			if (this->nodes[i] == node) {
+				outNodeID = i;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/// Returns false if no edge can be found leaving `outEdge` untouched.
+	/// If an edge is found, true is returned and `outEdge` is set to the
+	/// corresponding edge data.
+	bool FindEdgeBetweenNodes(NodeID from, NodeID to, EdgeData& outEdge) {
+		for (size_t i = 0; i < this->edges.size(); i++) {
+			if ((this->edges[i].from == from) && this->edges[i].to == to) {
+				outEdge = this->edges[i].data;
+				return true;
+			}
+		}
+		return false;
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <vector>
 #include "SDL2/SDL.h"
+#include "graph.h"
+
+typedef std::string AirportName;
+typedef double Distance;
 
 int main(int argc, char* argv[]) {
-	SDL_Init(SDL_INIT_VIDEO);
 
-	std::cout << "Hello World!" << std::endl;
+	Graph<AirportName, Distance> airport_network = { "SCE", "PHL", "EWR", "ORD" };
+	airport_network.AddNonDirectionalEdge(0, 1, 160.0);
+	airport_network.AddNonDirectionalEdge(0, 2, 220.0);
+	airport_network.AddNonDirectionalEdge(0, 3, 640.0);
+
+	double dist_sce_ord = 0.0;
+	if (airport_network.FindEdgeBetweenNodes(3, 0, dist_sce_ord)) {
+		std::cout << "Distance from ORD to SCE is " << dist_sce_ord << " miles." << std::endl;
+	} else {
+		std::cout << "No edge from ORD to SCE." << std::endl;
+	}
 
 	return 0;
 }
